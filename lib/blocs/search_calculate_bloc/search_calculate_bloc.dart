ddf0e16d74d420b7fefed3ad63659bab5f0e4d1d
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-04-29 19:38:29 -0500

    Update Cats

--- lib/blocs/search_calculate_bloc/search_calculate_bloc.dart
+++ lib/blocs/search_calculate_bloc/search_calculate_bloc.dart
@@ -9,45 +9,50 @@ import '../../repositories/cats_respository.dart';
 import '../search_cat/search_cat_bloc.dart';
 
 part 'search_calculate_event.dart';
+
 part 'search_calculate_state.dart';
 
-class SearchCalculateBloc extends Bloc<SearchCalculateEvent, SearchCalculateState> {
-
+class SearchCalculateBloc
+    extends Bloc<SearchCalculateEvent, SearchCalculateState> {
   late StreamSubscription catsSearchSubscription;
 
   final CatsRepository catsRepository;
 
-  final SearchCatBloc  searchCatBloc;
+  final SearchCatBloc searchCatBloc;
 
-
-  SearchCalculateBloc({required this.catsRepository, required this.searchCatBloc}) : super(SearchCalculateState.initial()) {
-
-    catsSearchSubscription =  searchCatBloc.stream.listen((SearchCatState searchCatState) {
+  SearchCalculateBloc(
+      {required this.catsRepository, required this.searchCatBloc})
+      : super(SearchCalculateState.initial()) {
+    catsSearchSubscription =
+        searchCatBloc.stream.listen((SearchCatState searchCatState) {
       _setFilterCats();
     });
 
     on<SearchCalculateFiltertEvent>((event, emit) {
-      emit(state.copyWith(filterCats: event.filterCats, status: CatsStatus.complete, searchTerm: event.termLetter));
+      emit(state.copyWith(
+          filterCats: event.filterCats,
+          status: CatsStatus.complete,
+          searchTerm: event.termLetter));
     });
   }
 
   Future<void> _setFilterCats() async {
     final String termLetter = searchCatBloc.state.searchTerm;
-    if(termLetter.isEmpty) return;
+    if (termLetter.isEmpty) return;
     emit(state.copyWith(status: CatsStatus.loading));
     try {
-      final List<dynamic> searchCats = await catsRepository.findCats(termLetter);
-      add(SearchCalculateFiltertEvent(filterCats: searchCats, termLetter: termLetter));
+      final List<dynamic> searchCats =
+          await catsRepository.findCats(termLetter);
+      add(SearchCalculateFiltertEvent(
+          filterCats: searchCats, termLetter: termLetter));
     } on CustomError catch (e) {
       emit(state.copyWith(status: CatsStatus.error, error: e));
     }
   }
 
   @override
-  Future<void> close(){
+  Future<void> close() {
     catsSearchSubscription.cancel();
     return super.close();
   }
-
 }
-
